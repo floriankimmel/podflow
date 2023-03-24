cd82213d575aa0702c809f1d120c96864312b782
Florian Kimmel <florianmkimmel@gmail.com>
2023-03-24 08:45:55 +0100

    Extract apiKey from 1password

--- src/create_blogpost_command.sh
+++ src/create_blogpost_command.sh
@@ -5,19 +5,21 @@ slug=${args[--slug]}
 
 fullPostTitle="LEP#$postNumber - $postTitle"
 
-json=$(curl  -s -X POST https://laufendentdecken-podcast.at/wp-json/podlove/v2/episodes --header "Authorization: Basic ZmxvcmlhbjpkUUNZIG1BOGYgM0p1cyBEcjJ2IDlIZXAgb2p1Yg==")
+apiKey=$(op item get "PodloveApiKey" --format json | jq -r '. | .fields | .[] | select(.label=="password") | .value')
+
+json=$(curl  -s -X POST https://laufendentdecken-podcast.at/wp-json/podlove/v2/episodes --header "Authorization: Basic $apiKey")
 
 episodeId=$(echo $json | jq -r ' . | "\(.id)"')
 response=$(curl -s -X POST https://laufendentdecken-podcast.at/wp-json/podlove/v2/episodes/$episodeId \
-    --header "Authorization: Basic ZmxvcmlhbjpkUUNZIG1BOGYgM0p1cyBEcjJ2IDlIZXAgb2p1Yg==" \
+    --header "Authorization: Basic $apiKey" \
     --header 'Content-Type: application/json; charset=utf-8' \
     -d "{ \"slug\": \"$slug\", \"title\": \"$fullPostTitle\", \"number\": \"$postNumber\" }")
 
-json=$(curl -s -X GET https://laufendentdecken-podcast.at/wp-json/podlove/v2/episodes/$episodeId --header "Authorization: Basic ZmxvcmlhbjpkUUNZIG1BOGYgM0p1cyBEcjJ2IDlIZXAgb2p1Yg==")
+json=$(curl -s -X GET https://laufendentdecken-podcast.at/wp-json/podlove/v2/episodes/$episodeId --header "Authorization: Basic $apiKey")
 postId=$(echo $json | jq -r ' . | "\(.post_id)"')
 
 response=$(curl -s -X POST https://laufendentdecken-podcast.at/wp-json/wp/v2/episodes/$postId \
-    --header "Authorization: Basic ZmxvcmlhbjpkUUNZIG1BOGYgM0p1cyBEcjJ2IDlIZXAgb2p1Yg==" \
+    --header "Authorization: Basic $apiKey" \
     --header 'Content-Type: application/json; charset=utf-8' \
     --data-raw "{ \"title\":\"$fullPostTitle\", \"status\": \"future\", \"date\": \"$postDate 09:00:00\", \"slug\": \"$postNumber\" }")
 
