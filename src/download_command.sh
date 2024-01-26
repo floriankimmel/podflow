519db0a4309f0dab9811270e01129df68181adde
Florian Kimmel <florianmkimmel@gmail.com>
2024-01-26 08:15:55 +0100

    Download to new recoring folder

--- src/download_command.sh
+++ src/download_command.sh
@@ -1,4 +1,4 @@
 file=${args[--file]}
 folder=$(echo $file | awk -F '_' '{print $2}' | cut -d '.' -f 1)
 
-curl https://rssfeed.laufendentdecken-podcast.at/data/$file --output ~/Dropbox/Resources/Podcast/Aufnahmen/$folder/$file
+curl https://rssfeed.laufendentdecken-podcast.at/data/$file --output "/Users/fkimmel/Dropbox/01Projects/EpisodenProduktion/Aufnahmen/$folder/$file"
